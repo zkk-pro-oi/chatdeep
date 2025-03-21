cfab253806caea3ea3429a16a66b7dbe13fb0893
zkk-pro-oi <3120506774@qq.com>
2025-03-21 17:33:12 +0800

    Add files via upload

--- chat_Ds.py
+++ chat_Ds.py
@@ -1,17 +1,13 @@
-
 from langchain.chains import ConversationChain
 from langchain_deepseek import ChatDeepSeek
 
+import os
 from langchain.memory import ConversationBufferMemory
-def get_chat_response(prompt,memory,openai_api_key):
-    model = ChatDeepSeek(model="deepseek-chat",openai_api_key=openai_api_key)
-    import os
-    os.environ['DEEPSEEK_API_KEY'] = openai_api_key
-    chain = ConversationChain(llm=model,memory=memory)
 
-    response = chain.invoke({"input":prompt})
+
+def get_chat_response(prompt, memory, deepseek_api_key):
+    model = ChatDeepSeek(model="deepseek-chat", api_key=deepseek_api_key)
+    chain = ConversationChain(llm=model, memory=memory)
+
+    response = chain.invoke({"input": prompt})
     return response["response"]
-
-memory = ConversationBufferMemory(return_messages=True)
-# print(get_chat_response("牛顿提出过哪些知名的定律",memory,os.getenv("DEEPSEEK_API_KEY")))
-# print(get_chat_response("我上一个问题是什么",memory,os.getenv("DEEPSEEK_API_KEY")))
